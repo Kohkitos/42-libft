e74fd67b4eaa2f4b7c700d6ac68dab25ab3c77cc
Kohkitos <sanzyagain@gmail.com>
2023-12-27 12:07:42 +0100

    tolower added

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/26 13:06:42 by fsanz-go          #+#    #+#              #
-#    Updated: 2023/12/27 12:05:12 by fsanz-go         ###   ########.fr        #
+#    Updated: 2023/12/27 12:07:25 by fsanz-go         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -39,7 +39,8 @@ SRC = ft_isalpha.c \
 		ft_isascii.c \
 		ft_isprint.c \
 		ft_strlen.c \
-		ft_toupper.c
+		ft_toupper.c \
+		ft_tolower.c
 
 OBJ_NAME = $(SRC:%.c=%.o)
 
--- src/ft_tolower.c
+++ src/ft_tolower.c
@@ -1,27 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/26 13:30:03 by fsanz-go          #+#    #+#             */
-/*   Updated: 2023/12/27 12:07:14 by fsanz-go         ###   ########.fr       */
+/*   Created: 2023/12/27 12:06:25 by fsanz-go          #+#    #+#             */
+/*   Updated: 2023/12/27 12:06:51 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stdlib.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-
-#endif
+int	ft_tolower(int c)
+{
+	return (c + ('a' - 'A'));
+}
