765bc56b46adb6e0ae2dc9f8511172f7c3e846e4
Kohkitos <sanzyagain@gmail.com>
2023-12-27 11:57:32 +0100

    Makefile and libft.h modified

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/26 13:06:42 by fsanz-go          #+#    #+#              #
-#    Updated: 2023/12/26 13:36:13 by fsanz-go         ###   ########.fr        #
+#    Updated: 2023/12/27 11:57:01 by fsanz-go         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -37,7 +37,8 @@ SRC = ft_isalpha.c \
 		ft_isdigit.c \
 		ft_isalnum.c \
 		ft_isascii.c \
-		ft_isprint.c
+		ft_isprint.c \
+		ft_strlen.c
 
 OBJ_NAME = $(SRC:%.c=%.o)
 
--- inc/libft.h
+++ inc/libft.h
@@ -6,17 +6,20 @@
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 13:30:03 by fsanz-go          #+#    #+#             */
-/*   Updated: 2023/12/26 13:32:46 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2023/12/27 11:56:39 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdlib.h>
+
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
