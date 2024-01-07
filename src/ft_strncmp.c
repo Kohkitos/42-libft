2bfd069dea2bdce6b4c011d6b5595ac2436cb2b6
Kohkitos <sanzyagain@gmail.com>
2024-01-07 15:40:38 +0100

    strncmp simplified

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 14:39:55 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/07 15:07:28 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/07 15:32:34 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		if (s1[i] == s2[i] && s1[i] && s2[i])
 			i++;
 		else
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
