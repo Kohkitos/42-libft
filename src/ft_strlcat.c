967a541c9c34e122d7fcb5a38943e98e7af61062
Kohkitos <sanzyagain@gmail.com>
2024-01-07 13:49:33 +0100

    strlcat fixed

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 12:47:40 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/07 13:42:21 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/07 13:48:06 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	i;
 	size_t	r;
 	size_t	size_d;
-	size_t	size_ss;
+	size_t	size_s;
 
-	size_ss = size + ft_strlen(src);
+	size_s = ft_strlen(src);
 	size_d = ft_strlen(dst);
-	if (size < size_d)
-		return (size_ss);
-	i = 0;
-	r = size_d + ft_strlen(src);
-	while ((i <= (size - size_d - 1)) && (*src))
+	if (size <= size_d)
+		return (size_s + size);
+	i = size_d;
+	r = size_d + size_s;
+	while ((i < (size - 1)) && (*src))
 	{
-		dst[size_d + i] = *src;
+		*(dst + i) = *src;
 		src++;
 		i++;
 	}
-	dst[i] = 0;
+	*(dst + i) = '\0';
 	return (r);
 }
