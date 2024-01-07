55c5dc4584350e4d5395b97193f925820683b402
Kohkitos <sanzyagain@gmail.com>
2024-01-07 15:08:36 +0100

    strncmp added

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/26 13:06:42 by fsanz-go          #+#    #+#              #
-#    Updated: 2024/01/07 14:31:07 by fsanz-go         ###   ########.fr        #
+#    Updated: 2024/01/07 14:44:01 by fsanz-go         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -49,7 +49,8 @@ SRC = ft_isalpha.c \
 		ft_memmove.c \
 		ft_strlcat.c \
 		ft_strchr.c \
-		ft_strrchr.c
+		ft_strrchr.c \
+		ft_strncmp.c
 
 OBJ_NAME = $(SRC:%.c=%.o)
 
---
+++ src/ft_strncmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/07 14:39:55 by fsanz-go          #+#    #+#             */
+/*   Updated: 2024/01/07 15:07:28 by fsanz-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+			i++;
+		else
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
+}
