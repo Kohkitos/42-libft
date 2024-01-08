e85e42142946b1102adada1d69f137d3f46f09b6
Francisco Javier Sanz Gonzalez <fsanz-go@c3r2s6.42madrid.com>
2024-01-08 16:47:41 +0100

    memcpy fixed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: fsanz-go <fsanz-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 18:57:45 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/07 19:13:16 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/08 16:47:17 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*d_cpy;
 	unsigned char	*s_cpy;
 
+	if (!dest && !src)
+		return (dest);
 	d_cpy = dest;
 	s_cpy = (unsigned char *)src;
 	while (n > 0)
