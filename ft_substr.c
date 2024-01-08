eaf63343b84b03ff87797f2bf65f5e3186f3f204
Francisco Javier Sanz Gonzalez <fsanz-go@c2r13s2.42madrid.com>
2024-01-08 12:27:56 +0100

    substr updated

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fsanz-go <fsanz-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 11:43:59 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/08 12:02:44 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/08 12:26:28 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
+	size_t	i;
 
-	ptr = malloc(sizeof(char) * len);
+	ptr = malloc(sizeof(char) * (len + 1));
 	if (!ptr)
 		return (NULL);
-	ft_memcpy(ptr, s, len);
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
 	return (ptr);
 }
