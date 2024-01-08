ed9e452b5f87cfe131483aded6744a43ec85aeae
Kohkitos <sanzyagain@gmail.com>
2024-01-08 22:33:24 +0100

    strtrim finished

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,32 +6,25 @@
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 12:40:23 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/08 21:55:59 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/08 22:31:45 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	start_end(char const *s1, char const *set, size_t i, size_t j)
+static size_t	in_set(char const *set, char c)
 {
-	size_t	res;
-	size_t	set_len;
-
-	res = 0;
-	set_len = ft_strlen(set);
-	while (i < j)
+	while (*set)
 	{
-		if (ft_strnstr(set, ft_substr(s1, i, 1), set_len))
-			res++;
-		i++;
+		if (*set == c)
+			return (1);
+		set++;
 	}
-	return (res);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	set_len;
-	size_t	s1_len;
 	size_t	start;
 	size_t	end;
 
@@ -39,9 +32,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	if (!set)
 		return (ft_strdup(s1));
-	set_len = ft_strlen(set);
-	s1_len = ft_strlen(s1);
-	start = start_end(s1, set, 0, set_len);
-	end = start_end(s1, set, s1_len - set_len, s1_len);
-	return (ft_substr(s1, start, s1_len - start - end));
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && in_set(set, s1[start]))
+		start++;
+	while (end > start && in_set(set, s1[end - 1]))
+		end--;
+	return (ft_substr(s1, start, end - start));
 }
