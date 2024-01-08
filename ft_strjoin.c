4b30a48d2b5ea1e1b8734976b1569802147e6725
Francisco Javier Sanz Gonzalez <fsanz-go@c3r2s6.42madrid.com>
2024-01-08 17:18:29 +0100

    strjoin fixed

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fsanz-go <fsanz-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 12:17:26 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/08 12:22:09 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/08 17:18:00 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len_s2;
 	char	*ptr;
 
+	if (!s1 && !s2)
+		return (NULL);
+	else if (!s1)
+		return (ft_strdup(s2));
+	else if (!s2)
+		return (ft_strdup(s1));
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
-	ptr = malloc(sizeof(char) * (len_s1 + len_s2));
+	ptr = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 	if (!ptr)
 		return (NULL);
-	ft_memcpy(ptr, s1, len_s1);
-	ft_memcpy((ptr + len_s1), s2, len_s2);
+	ft_strlcpy(ptr, s1, len_s1 + 1);
+	ft_strlcpy(ptr + len_s1, s2, len_s2 + 1);
 	return (ptr);
 }
