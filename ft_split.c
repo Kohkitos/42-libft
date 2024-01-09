49ab81b7dcfcd07a272973ed668b922195821b16
Francisco Javier Sanz Gonzalez <fsanz-go@c3r11s6.42madrid.com>
2024-01-09 18:12:04 +0100

    split finished

--- ft_split.c
+++ ft_split.c
@@ -3,72 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: fsanz-go <fsanz-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 10:11:22 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/09 10:55:52 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/09 18:10:35 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	**free_arr(char **arr, size_t j)
+static char	**free_all(char **arr, size_t i)
 {
-	while (j > 0)
+	while (i > 0)
 	{
-		j--;
-		free(arr[j]);
+		i--;
+		free(arr[i]);
 	}
 	free(arr);
 	return (NULL);
 }
 
-static char	**assemble(char	**arr, char const *s, char c, size_t n)
+static size_t	string_n(char const *s, char c)
+{
+	size_t	n;
+	size_t	flag;
+
+	n = 0;
+	flag = 0;
+	while (*s)
+	{
+		if (*s == c)
+			flag = 0;
+		else if (flag == 0)
+		{
+			n++;
+			flag = 1;
+		}
+		s++;
+	}
+	return (n);
+}
+
+static char	**assemble(char **res, char const *s, char c, size_t n)
 {
 	size_t	i;
 	size_t	j;
-	size_t	k;
-	size_t	len;
 
 	i = 0;
-	j = 0;
-	while (j < n && s[i])
+	while (i < n && *s)
 	{
-		while (s[i] == c)
-			i++;
-		k = i;
-		while (s[i] != c)
+		j = 0;
+		while (*s == c)
+			s++;
+		while (s[j] != c && s[j])
+			j++;
+		if (j)
 		{
-			len++;
-			i++;
+			res[i] = ft_substr(s, 0, j);
+			if (!res)
+				return (free_all(res, i));
 		}
-		arr[j] = ft_substr(s, k, len);
-		if (!arr[j])
-			return (free_arr(arr, j));
-		j++;
+		s += j;
+		i++;
 	}
-	arr[j] = 0;
-	return (arr);
+	res[i] = 0;
+	return (res);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**arr;
+	char	**res;
 	size_t	n;
-	size_t	i;
 
 	if (!s)
 		return (NULL);
-	n = 1;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			n++;
-		i++;
-	}
-	arr = malloc(sizeof(char *) * n + 1);
-	if (!arr)
+	n = string_n(s, c);
+	res = malloc(sizeof(char *) * (n + 1));
+	if (!res)
 		return (NULL);
-	return (assemble(arr, s, c, n));
+	return (assemble(res, s, c, n));
 }
