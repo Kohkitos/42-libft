31336db6877e173c0555e1cdff63b0ec4527e1ff
Kohkitos <sanzyagain@gmail.com>
2024-01-12 15:50:05 +0100

    lstmap updated

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 10:44:21 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/12 12:51:20 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/12 15:48:34 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*res;
 	t_list	*aux;
+	char	*tmp;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	res = ft_lstnew(f(lst -> content));
-	if (!res)
-	{
-		ft_lstclear(&res, del);
-		return (NULL);
-	}
-	lst = lst -> next;
+	res = NULL;
 	while (lst)
 	{
-		aux = ft_lstnew(f(lst -> content));
+		tmp = f(lst -> content);
+		aux = ft_lstnew(tmp);
 		if (!aux)
 		{
 			ft_lstclear(&res, del);
+			del(tmp);
 			return (NULL);
 		}
 		ft_lstadd_back(&res, aux);
