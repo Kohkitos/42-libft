a3458ad389eced692397828ca88384ee2bf65efe
Kohkitos <sanzyagain@gmail.com>
2024-01-12 17:13:55 +0100

    split modified

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsanz-go <fsanz-go@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 10:11:22 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/10 17:16:41 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/12 17:13:46 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static char	**free_all(char **res, size_t i)
 	return (NULL);
 }
 
-static size_t	string_n(char const *s, char c)
+static size_t	string_count(char const *s, char c)
 {
 	size_t	n;
 	size_t	flag;
@@ -54,14 +54,12 @@ static char	**assemble(char **res, char const *s, char c, size_t n)
 			s++;
 		while (s[j] != c && s[j])
 			j++;
-		if (j)
-		{
-			res[i] = ft_substr(s, 0, j);
-			if (!res[i])
-				return (free_all(res, i));
-		}
-		s += j;
+		res[i] = (char *)ft_calloc(sizeof(char), (j + 1));
+		ft_strlcpy(res[i], s, j + 1);
+		if (!res[i])
+			return (free_all(res, i));
 		i++;
+		s += j;
 	}
 	res[i] = 0;
 	return (res);
@@ -74,7 +72,7 @@ char	**ft_split(char const *s, char c)
 
 	if (!s || !*s)
 		return ((char **)ft_calloc(sizeof(char *), 1));
-	n = string_n(s, c);
+	n = string_count(s, c);
 	res = ft_calloc(sizeof(char *), (n + 1));
 	if (!res)
 		return (NULL);
