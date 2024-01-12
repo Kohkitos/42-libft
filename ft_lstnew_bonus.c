3200dbbddc4762c496765f973e1314cafc2415a2
Kohkitos <sanzyagain@gmail.com>
2024-01-12 09:49:38 +0100

    name changed

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 18:03:42 by fsanz-go          #+#    #+#             */
+/*   Updated: 2024/01/12 09:38:19 by fsanz-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node -> content = (void *)content;
+	node -> next = NULL;
+	return (node);
+}
